504a0d72cc46cab1d43117599f6ad3d9cade98f2
sandro <contact@sanqro.xyz>
2023-02-20 13:20:05 +0100

    add question for new round

--- main.c
+++ main.c
@@ -7,22 +7,28 @@ int main() {
     srand(time(0));
 
     printf("Welcome to Rock-Paper-Scissors!\n");
-    printf("Enter your choice (1 for Rock, 2 for Paper, 3 for Scissors): ");
-    scanf("%d", &player_choice);
 
-    computer_choice = rand() % 3 + 1;
+    do {
+        printf("Enter your choice (1 for Rock, 2 for Paper, 3 for Scissors): ");
+        scanf("%d", &player_choice);
 
-    printf("Computer chose: %d.\n", computer_choice);
+        computer_choice = rand() % 3 + 1;
 
-    if (player_choice == computer_choice) {
-        printf("It's a tie!\n");
-    } else if ((player_choice == 1 && computer_choice == 3) ||
-               (player_choice == 2 && computer_choice == 1) ||
-               (player_choice == 3 && computer_choice == 2)) {
-        printf("You win!\n");
-    } else {
-        printf("Computer wins!\n");
-    }
+        printf("Computer chose: %d.\n", computer_choice);
+
+        if (player_choice == computer_choice) {
+            printf("It's a tie!\n");
+        } else if ((player_choice == 1 && computer_choice == 3) ||
+                   (player_choice == 2 && computer_choice == 1) ||
+                   (player_choice == 3 && computer_choice == 2)) {
+            printf("You win!\n");
+        } else {
+            printf("Computer wins!\n");
+        }
+        
+        printf("Do you want to play again? (1 for Yes, 0 for No): ");
+        scanf("%d", &player_choice);
+    } while (player_choice == 1);
 
     return 0;
 }
